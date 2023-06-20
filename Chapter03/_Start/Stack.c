614b50ccaa3b494c4262afb2ac8144ed76b6d5e6
Kautzi <alexanderkautz.ak@gmail.com>
2023-06-20 07:32:47 +0200

    NEXT 13

--- Chapter03/_Start/Stack.c
+++ Chapter03/_Start/Stack.c
@@ -1,16 +1,63 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdlib.h>//malloc,calloc,realloc
 
 #include "Stack.h"
 
 stack_t *createStack(const uint32_t capacity)
 {
+    stack_t *array = (stack_t*) malloc(sizeof(stack_t));
+
+    if(array == NULL)
+    {
+        return NULL;
+    }
+
+    value_type_t *data = (value_type_t*)malloc(sizeof(value_type_t)*capacity);
+
+    if(data == NULL)
+    {
+        free(array);
+        return NULL;
+    }
+
+    array->data=data;
+
+    array->size=0U;
+    array->capacity=capacity;
+
+    return array;
+
 }
 
 stack_t *freeStack(stack_t *stack)
 {
+    if(stack == NULL)
+    {
+        return NULL;
+    }
+
+    if(stack->data != NULL)
+    {
+    free(stack->data);
+    stack->data=NULL;
+    }
+
+    free(stack);
+    stack=NULL;
+
+    return NULL;
 }
 
 void printStack(const stack_t *const stack)
 {
+    if(stack == NULL )
+    {
+        return;
+    }
+
+    printf("Has capacity => %u\n",stack->capacity);
+
+    return;
+
+
 }
--- Chapter03/_Start/Stack.h
+++ Chapter03/_Start/Stack.h
@@ -12,10 +12,26 @@
 /* DEFINES AND TYPES  */
 /**********************/
 
+typedef float value_type_t;
+
+#define NO_VALUE (value_type_t)(INFINITY)
+
+typedef struct stack
+{
+    uint32_t size;
+    uint32_t capacity;
+    value_type_t *data;
+
+}stack_t;
 
 /**********************/
 /*     FUNCTIONS      */
 /**********************/
 
+stack_t *createStack(const uint32_t capacity);
+
+stack_t *freeStack(stack_t *stack);
+
+void printStack(const stack_t *const stack);
 
 #endif // STACK_H
